b5b430ee84a1ca3b5a39f0756353cc94e285ace3
ArisDEV <tranthanhnam.ccc@gmail.com>
2024-10-19 11:40:40 +0700

    default to port 10000

--- websivi.py
+++ websivi.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def getapp() -> str:
     return "Hello World!"
 
-def run_application() -> None:
-    app.run()
+def run_application(port=10000) -> None:
+    app.run(port=port)

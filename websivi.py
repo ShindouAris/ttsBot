bf69a12bcf36fe304193f144d8053b1bb5113a5a
ArisDEV <tranthanhnam.ccc@gmail.com>
2024-10-19 11:54:07 +0700

    add host parameter, set debug to false

--- websivi.py
+++ websivi.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 def getapp() -> str:
     return "Hello World!"
 
-def run_application(port=10000) -> None:
-    app.run(port=port)
+def run_application(port=10000, host =  "0.0.0.0") -> None:
+    app.run(port=port, host=host, debug=False)

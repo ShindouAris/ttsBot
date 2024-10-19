93d49fee67f4c525bb500c12d38d425eabbc7b76
ArisDEV <tranthanhnam.ccc@gmail.com>
2024-10-19 22:20:22 +0700

    add help cog + help command

---
+++ Module/help.py
@@ -0,0 +1,32 @@
+import disnake
+from disnake.ext import commands
+from utils.ClientUser import ClientUser
+
+class Help(commands.Cog):
+    def __init__(self, bot):
+        self.bot: ClientUser = bot
+
+    @commands.command(name="help", description="Hiển thị tin nhắn này", aliases=["h"])
+    async def help(self, ctx: disnake.ApplicationCommandInteraction):
+        list_cmds: list = []
+        embed = disnake.Embed()
+        embed.title = "Trợ giúp lệnh"
+        txt = ""
+        extra = ""
+
+        for cmd in self.bot.commands:
+            list_cmds.append(cmd)
+
+        for cmd in list_cmds:
+            if cmd.aliases:
+                items: list[str] = []
+                for item in cmd.aliases:
+                    items.append(item)
+                extra = f"- Aliases: `{items}`"
+            txt += f"**{cmd.name.capitalize()} {extra}**\n```{cmd.description}```\n"
+
+        embed.description = txt
+        await ctx.send(embed=embed)
+
+def setup(bot: ClientUser):
+    bot.add_cog(Help(bot))
